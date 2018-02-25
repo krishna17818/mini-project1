01c82e46ae733b06837f0de2c1dcaf0c3320e57f
krishna <kp537@njit.edu>
2018-02-25 00:44:06 -0500

    This is register assignment.

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/card', function () {
+    return view('card');
+});
+Route::get('/viewmessage', function () {
+    return view('viewmessage');
+});
+Route::get('/logout', function () {
+    return view('logout');
+});
+
